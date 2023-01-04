431685b78c46a95894d670d1fd04c9b25bdd3e4f
Adam Brower-Lingsch <abrowerlingsch@gmail.com>
2023-01-04 11:09:21 -0500

    first commit

---
+++ js/main.js
@@ -0,0 +1,6 @@
+function changeImage()
+        {
+        var img = document.getElementById("image");
+        img.src="http://photos1.blogger.com/blogger/761/1659/1600/budgetts%20frog_jacobs-ecology1co1uk.jpg";
+        return false;
+        }
